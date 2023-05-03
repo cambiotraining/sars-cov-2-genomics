ad96af072384f0d9d526b0914352b023aaeac7c8
Hugo Tavares <hm533@cam.ac.uk>
2023-05-03 17:41:38 +0100

    update phylogeny script

--- utils/eqa/scripts_nanopore/05-phylogeny.sh
+++ utils/eqa/scripts_nanopore/05-phylogeny.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# create outut directories
+mkdir results/mafft
+mkdir results/iqtree
+
+# combine sequences
+cat report/consensus.fa resources/eqa_collaborators/eqa_consensus.fa > results/mafft/unaligned_consensus.fa
+
 # alignment
-mkdir -p results/mafft
-mafft --6merpair --maxambiguous 0.2 --addfragments report/consensus.fa resources/reference/sarscov2.fa > results/mafft/alignment.fa
+mafft --6merpair --maxambiguous 0.2 --addfragments results/mafft/unaligned_consensus.fa resources/reference/sarscov2.fa > results/mafft/alignment.fa
 
 # tree inference
-mkdir -p results/iqtree
 iqtree2 -s results/mafft/alignment.fa --prefix results/iqtree/consensus
